bc6b15fb93faed935ceb4be1576918406943bd4c
anvartdinovtimurlinux <Anvartdinovtimurlinux@gmail.com>
2022-05-18 19:59:23 +0300

    Lesson 2, Task 3. The task is done.

---
+++ lesson_2/task_3/task_3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+struct Address
+{
+    std::string country;
+    std::string city;
+    std::string street;
+    int house;
+    int apartment;
+    int index;
+};
+
+void printAddress(Address &address)
+{
+    std::cout << "Страна: " << address.country << std::endl;
+    std::cout << "Город: " << address.city << std::endl;
+    std::cout << "Улица: " << address.street << std::endl;
+    std::cout << "Дом: " << address.house << std::endl;
+    std::cout << "Квартира: " << address.apartment << std::endl;
+    std::cout << "Индекс: " << address.index << std::endl;
+}
+
+int main()
+{
+//    В динамической памяти
+    Address* address_1 = new Address {"Россия", "Москва", "Арбат", 14, 21, 123053};
+    printAddress(*address_1);
+    delete address_1;
+
+    std::cout << std::endl << std::endl;
+//    В стеке
+    Address address_2 = Address {"Россия", "Тула", "Строителей", 15, 1, 153436};
+    printAddress(address_2);
+
+    return 0;
+}
